18521741a61bebc4b07db0838672713fea3d2f16
Luca Giovenzana <luca@giovenzana.org>
2023-03-10 11:54:03 +0100

    feat(agile-distilled): tic tac toe draw

--- src/tic-tac-toe/tic-tac-toe.spec.ts
+++ src/tic-tac-toe/tic-tac-toe.spec.ts
@@ -1,7 +1,8 @@
 class TicTacToe {
     private firstPlayerTurn: boolean = true
+    private turn: number = 1
     private board: sign[][] = new Array(3)
-    private winner: 1 | 2 | undefined = undefined
+    private winner: 1 | 2 | undefined | null = undefined
 
     constructor() {
         for (let row = 0; row < 3; row++) {
@@ -20,17 +21,22 @@ class TicTacToe {
 
     private changeTurn() {
         this.firstPlayerTurn = !this.firstPlayerTurn
+        this.turn += 1
     }
 
     getSign(row: number, column: number) {
         return this.board[row][column]
     }
 
-    getWinner(): 1 | 2 | undefined {
+    getWinner(): 1 | 2 | undefined | null{
         return this.winner
     }
 
     private checkWinner() {
+        if(this.turn === 9) {
+            this.winner = null
+            return
+        }
         this.winner = this.firstPlayerTurn ? 1 : 2
     }
 }
@@ -41,81 +47,119 @@ enum sign {
 }
 
 describe('TicTacToe', function () {
-    describe('Given the game just started', function () {
-        const ticTacToe = new TicTacToe()
-        describe('When the first player plays', function () {
+
+    describe('Scenario: First sign is X second O', function () {
+        describe('Given the game just started', function () {
+            const ticTacToe = new TicTacToe()
+            describe('When the first player plays', function () {
+                ticTacToe.drawTo(0, 0)
+                it('Then it should be X ', function () {
+                    expect(ticTacToe.getSign(0, 0)).toEqual(sign.X)
+                });
+            });
+        });
+        describe('Given the first player played', function () {
+            const ticTacToe = new TicTacToe()
             ticTacToe.drawTo(0, 0)
-            it('Then it should be X ', function () {
-                expect(ticTacToe.getSign(0, 0)).toEqual(sign.X)
+            describe('When the second player plays', function () {
+                ticTacToe.drawTo(0, 1)
+                it('Then it should be O ', function () {
+                    expect(ticTacToe.getSign(0, 1)).toEqual(sign.O)
+                });
             });
         });
     });
-    describe('Given the first player played', function () {
-        const ticTacToe = new TicTacToe()
-        ticTacToe.drawTo(0, 0)
-        describe('When the second player plays', function () {
-            ticTacToe.drawTo(0, 1)
-            it('Then it should be O ', function () {
-                expect(ticTacToe.getSign(0, 1)).toEqual(sign.O)
-            });
-        });
-    });
-    describe('Given the first player played in 0, 0', function () {
-        const ticTacToe = new TicTacToe()
-        ticTacToe.drawTo(0, 0)
 
-        describe('When the second player plays in 0,0 ', function () {
-            it('Then it should return an error', function () {
-                expect(() => {
-                    ticTacToe.drawTo(0, 0)
-                }).toThrow()
+    describe('Scenario: Cannot sign twice the same', function () {
+        describe('Given the first player played in 0, 0', function () {
+            const ticTacToe = new TicTacToe()
+            ticTacToe.drawTo(0, 0)
+
+            describe('When the second player plays in 0,0 ', function () {
+                it('Then it should return an error', function () {
+                    expect(() => {
+                        ticTacToe.drawTo(0, 0)
+                    }).toThrow()
+                });
             });
         });
     });
-    describe('Given first player with 2 sign in a row', function () {
-        // X | O | O |
-        // X |   |   |
-        //   |   |   |
 
-        const ticTacToe = new TicTacToe()
-        ticTacToe.drawTo(0, 0) // player 1
-        ticTacToe.drawTo(0, 1) // player 2
-        ticTacToe.drawTo(1, 0) // player 1
-        ticTacToe.drawTo(0, 2) // player 2
-        describe('When the first player place the 3rd sign in a row', function () {
+    describe('Scenario: Player with three sign in a row wins', function () {
+        describe('Given the first player with 2 sign in a row', function () {
             // X | O | O |
             // X |   |   |
-            // X |   |   | <-
+            //   |   |   |
 
-            ticTacToe.drawTo(2, 0) // player 1
+            const ticTacToe = new TicTacToe()
+            ticTacToe.drawTo(0, 0) // player 1
+            ticTacToe.drawTo(0, 1) // player 2
+            ticTacToe.drawTo(1, 0) // player 1
+            ticTacToe.drawTo(0, 2) // player 2
+            describe('When the first player place the 3rd sign in a row', function () {
+                // X | O | O |
+                // X |   |   |
+                // X |   |   | <-
 
-            it('Then it should win', function () {
-                expect(ticTacToe.getWinner()).toEqual(1)
+                ticTacToe.drawTo(2, 0) // player 1
+
+                it('Then it should win', function () {
+                    expect(ticTacToe.getWinner()).toEqual(1)
+                });
+            });
+        });
+        describe('Given the second player with 2 sign in a row', function () {
+            // O | X | X |
+            // O |   |   |
+            //   |   | X |
+
+            const ticTacToe = new TicTacToe()
+            ticTacToe.drawTo(0, 1) // player 1
+            ticTacToe.drawTo(0, 0) // player 2
+            ticTacToe.drawTo(0, 2) // player 1
+            ticTacToe.drawTo(1, 0) // player 2
+            ticTacToe.drawTo(2, 2) // player 1
+            describe('When the second place the 3rd sign in a row', function () {
+                // O | X | X |
+                // O |   |   |
+                // O |   | X | <-
+
+                ticTacToe.drawTo(2, 0) // player 2
+
+                it('Then it should win', function () {
+                    expect(ticTacToe.getWinner()).toEqual(2)
+                });
             });
         });
     });
-    describe('Given the second player with 2 sign in a row', function () {
-        // O | X | X |
-        // O |   |   |
-        //   |   | X |
-
-        const ticTacToe = new TicTacToe()
-        ticTacToe.drawTo(0, 1) // player 1
-        ticTacToe.drawTo(0, 0) // player 2
-        ticTacToe.drawTo(0, 2) // player 1
-        ticTacToe.drawTo(1, 0) // player 2
-        ticTacToe.drawTo(2, 2) // player 1
-        describe('When the second place the 3rd sign in a row', function () {
+    describe('Scenario: All places used with no winner', function () {
+        describe('Given players used all but one places', function () {
             // O | X | X |
-            // O |   |   |
-            // O |   | X | <-
+            // O | X | O |
+            //   | O | X |
 
-            ticTacToe.drawTo(2, 0) // player 2
+            const ticTacToe = new TicTacToe()
+            ticTacToe.drawTo(0, 1) // player 1
+            ticTacToe.drawTo(0, 0) // player 2
+            ticTacToe.drawTo(0, 2) // player 1
+            ticTacToe.drawTo(1, 0) // player 2
+            ticTacToe.drawTo(2, 2) // player 1
+            ticTacToe.drawTo(2, 1) // player 2
+            ticTacToe.drawTo(1, 1) // player 1
+            ticTacToe.drawTo(1, 2) // player 2
+            describe('When the last place is used', function () {
+                // O | X | X |
+                // O | X | O |
+                //   | O | X | <-
 
-            it('Then it should win', function () {
-                expect(ticTacToe.getWinner()).toEqual(2)
+                ticTacToe.drawTo(2, 0) // player 1
+
+                it('Then game should end in a draw', function () {
+                    expect(ticTacToe.getWinner()).toEqual(null)
+                });
             });
         });
+
     });
 });
 
